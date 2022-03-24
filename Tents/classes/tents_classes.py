4517363444f9863066d804d911adbf9508f199b5
consigliere <thinh.nguyenbk2018@hcmut.edu.vn>
2022-03-24 18:20:27 +0700

    added implementation for heuristic search

--- Tents/classes/tents_classes.py
+++ Tents/classes/tents_classes.py
@@ -4,6 +4,22 @@
 from copy import copy, deepcopy
 from enum import Enum
 from typing import Dict, List, Set
+import heapq
+import functools
+
+# * Frontier for BFS strategy
+class QueueFrontier(StackFrontier):
+
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[0]
+            self.frontier = self.frontier[1:]
+            return node
+
+
+# * Frontier for DFS strategy
 
 
 class StackFrontier():
@@ -27,7 +43,26 @@ class StackFrontier():
             self.frontier = self.frontier[:-1]
             return node
 
+# * This frontier will keep an sorted list of nodes based on there heuristics function
+# * On removal, pop out the node with highest value of heuristics value
+# * On insert, calculate and push the node in its correct order
+# ==> Heap is suitable for this
 
+
+class A_Star_Frontier(StackFrontier):
+    def __init__(self):
+        self.frontier = []
+
+    def add(self, node):
+        wrapped_node = HeuristicNode(node)
+        heapq.heappush(self.frontier, wrapped_node)
+
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = heapq.heappop(self.frontier)
+            return node
 # Note: Index of the game start from 1
 
 
@@ -98,7 +133,8 @@ def get_node_char(node: PositionNode) -> str:
     elif isinstance(node, ConstraintNode):
         char = "C" + \
                str(node.col_index) if node.contraint_type == ConstraintNodeType.COL else "R" + \
-                                                                                         str(str(node.row_index))
+                                                                                         str(str(
+                                                                                             node.row_index))
         return char + ":" + str(node.tents_number_required)
     elif isinstance(node, TentNode):
         return "A"
@@ -111,8 +147,6 @@ def calculate_key(row_index: int, col_index: int) -> int:
 
 
 # Class defining an action
-
-
 class Action():
     # Put an tent on a position on the board
     def __init__(self, pos: PositionNode) -> None:
@@ -121,7 +155,7 @@ class Action():
     def print(self):
         print(
             f'A tent is placed on: {self.tent_pos.row_index} , {self.tent_pos.col_index}')
-    
+
     def __str__(self) -> str:
         return f'Tent to: ({self.tent_pos})'
 
@@ -134,8 +168,6 @@ class BoardStateType(Enum):
 
 
 # Containt the board state and main board functionalities
-
-
 class BoardNode():
     board_state: BoardStateType
 
@@ -147,27 +179,27 @@ class BoardNode():
             trees = {}
         if constraints is None:
             constraints = {}
-        # Useful positions saved for fast access: Not used currently 
+        # Useful positions saved for fast access: Not used currently
         self.diagonal_to_trees = {}
         self.straight_adjacent_to_trees = {}
         self.grid = board_grid
-        
+
         self.width = width
         self.height = height
-        
+
         self.tents_dict = tents
         self.trees_dict = trees
         self.possible_locations = board_grid
-        
+
         self.constraint_dict = constraints
-        # Keep the constraints for reviewing 
+        # Keep the constraints for reviewing
         temp = deepcopy(constraints)
-        self.constraint_set = set(temp.values()) 
-        
+        self.constraint_set = set(temp.values())
+
         self.board_state = BoardStateType.VALID
-        
+
         self.update_possible_location(init=True)
-        
+
         self.is_all_ok_shorcut_check = False
 
     # Hashsing and Equal allows the positnode to be used as dictionary key
@@ -195,18 +227,22 @@ class BoardNode():
 
             tree_dict_values = self.trees_dict.values()
             for tree in tree_dict_values:
-                diagonal_to_trees = diagonal_to_trees.union(set(self.get_diagonal_adjacent_nodes(tree).values()))
+                diagonal_to_trees = diagonal_to_trees.union(
+                    set(self.get_diagonal_adjacent_nodes(tree).values()))
 
                 straight_adjacent_to_trees = straight_adjacent_to_trees.union(
                     set(self.get_straight_adjacent_nodes(tree).values()))
 
-                adjacent_to_tree = adjacent_to_tree.union(set(self.get_adjacent_nodes(tree).values()))
+                adjacent_to_tree = adjacent_to_tree.union(
+                    set(self.get_adjacent_nodes(tree).values()))
 
             self.diagonal_to_trees = diagonal_to_trees
             self.straight_adjacent_to_trees = straight_adjacent_to_trees
-            pure_diagonal_to_tree = diagonal_to_trees.difference(straight_adjacent_to_trees)
+            pure_diagonal_to_tree = diagonal_to_trees.difference(
+                straight_adjacent_to_trees)
 
-            pos_to_remove = pos_to_remove.union(pure_diagonal_to_tree).union(tree_set)
+            pos_to_remove = pos_to_remove.union(
+                pure_diagonal_to_tree).union(tree_set)
 
             # Filter out the zeored column and row
             zeroed_contraints = [constraint for constraint in self.constraint_dict.values() if
@@ -215,12 +251,15 @@ class BoardNode():
             for con in zeroed_contraints:
                 if con.contraint_type == ConstraintNodeType.COL:
                     col_idx = con.col_index
-                    pos_to_remove = pos_to_remove.union(set(self.get_whole_col(col_idx)))
+                    pos_to_remove = pos_to_remove.union(
+                        set(self.get_whole_col(col_idx)))
                 else:
                     row_idx = con.row_index
-                    pos_to_remove = pos_to_remove.union(set(self.get_whole_row(row_idx)))
+                    pos_to_remove = pos_to_remove.union(
+                        set(self.get_whole_row(row_idx)))
 
-            possible_pos_set = set(self.possible_locations.values()).difference(pos_to_remove)
+            possible_pos_set = set(
+                self.possible_locations.values()).difference(pos_to_remove)
             possible_pos_set = possible_pos_set.intersection(adjacent_to_tree)
 
             self.possible_locations = dict(
@@ -229,25 +268,29 @@ class BoardNode():
         # Remove the ZEROED row and columns:
         if tent_node:
             self.place_tent_node_and_check_constraint(tent_node)
-            if self.board_state == BoardStateType.INVALID: 
-                return 
-            
+            if self.board_state == BoardStateType.INVALID:
+                return
+
             # Remove the adjacent nodes from the possible list
             adjacent_to_tents = self.get_adjacent_nodes(tent_node)
             self.remove_nodes_from_pos_list(adjacent_to_tents)
 
             # Update the state related to tree
             tree_set = set(self.trees_dict.values())
-            cross_adjacent_to_tent = set(self.get_straight_adjacent_nodes(tent_node).values())
+            cross_adjacent_to_tent = set(
+                self.get_straight_adjacent_nodes(tent_node).values())
 
-            possible_tree_binded = tree_set.intersection(cross_adjacent_to_tent)
+            possible_tree_binded = tree_set.intersection(
+                cross_adjacent_to_tent)
             if len(possible_tree_binded) == 1:
                 tree_binded = possible_tree_binded.pop()
                 key = hash(tree_binded)
                 self.trees_dict[key].is_properly_binded = True
-                adjacent_to_tree = self.get_straight_adjacent_nodes(tree_binded)
+                adjacent_to_tree = self.get_straight_adjacent_nodes(
+                    tree_binded)
                 for adjacent_node in adjacent_to_tree.values():
-                    adjacents_set = set(self.get_straight_adjacent_nodes(adjacent_node).values())
+                    adjacents_set = set(
+                        self.get_straight_adjacent_nodes(adjacent_node).values())
                     adjacents_set = adjacents_set.difference([tree_binded])
                     found_other_tree = False
                     for node in adjacents_set:
@@ -257,7 +300,8 @@ class BoardNode():
                     if not found_other_tree:
                         self.remove_nodes_from_pos_list([adjacent_node])
             # Check for invalid tree
-            diagonal_to_tent_set = set(self.get_diagonal_adjacent_nodes(tent_node).values())
+            diagonal_to_tent_set = set(
+                self.get_diagonal_adjacent_nodes(tent_node).values())
             tree_diag_to_tent = tree_set.intersection(diagonal_to_tent_set)
             for tree in tree_diag_to_tent:
                 if not set(self.get_straight_adjacent_nodes(tree).values()).intersection(set(self.possible_locations.values())):
@@ -271,44 +315,46 @@ class BoardNode():
             # If the number of possible location is equal to number of tents => Shorcut: Try place them and check for contraints
             if len(self.possible_locations) == trees_to_bind:
                 is_all_ok = False
-                for pos in self.possible_locations.values(): 
-                    is_all_ok = self.place_tent_node_and_check_constraint(pos,is_shorcut_checking=True) 
-                    if self.board_state == BoardStateType.INVALID: 
-                        return 
-                if is_all_ok == True: 
-                    self.board_state = BoardStateType.SOLVED 
-                
-    def place_tent_node_and_check_constraint(self, tent_node, is_shorcut_checking = False): 
-            if is_shorcut_checking: 
-                # Updating tents for visualization purpose 
-                tent_location = tent_node
-                tent = TentNode(tent_location.row_index, tent_location.col_index)
-                self.grid[hash(tent)] = tent
-                self.tents_dict[hash(tent)] = tent
-            # Decrease the allowed tents on the chosen node
-            constraint_row_node = self.get_row_constraint(tent_node)
-            constraint_col_node = self.get_col_constraint(tent_node)
-            if constraint_row_node.tents_number_required == 0:
-                self.board_state = BoardStateType.INVALID
-                return False
-            if constraint_col_node.tents_number_required == 0:
-                self.board_state = BoardStateType.INVALID
-                return False
-            constraint_col_node.tents_number_required -= 1
-            if constraint_col_node.is_satisfied():
-                self.remove_nodes_from_pos_list(self.get_whole_col(constraint_col_node.col_index))
-            constraint_row_node.tents_number_required -= 1
-            if constraint_row_node.is_satisfied():
-                self.remove_nodes_from_pos_list(self.get_whole_row(constraint_row_node.row_index))
+                for pos in self.possible_locations.values():
+                    is_all_ok = self.place_tent_node_and_check_constraint(
+                        pos, is_shorcut_checking=True)
+                    if self.board_state == BoardStateType.INVALID:
+                        return
+                if is_all_ok == True:
+                    self.board_state = BoardStateType.SOLVED
 
-            if len(self.tents_dict) > len(self.trees_dict):
-                self.board_state = BoardStateType.INVALID
-                return False 
-            
-            return True 
-            
+    def place_tent_node_and_check_constraint(self, tent_node, is_shorcut_checking=False):
+        if is_shorcut_checking:
+            # Updating tents for visualization purpose
+            tent_location = tent_node
+            tent = TentNode(tent_location.row_index,
+                            tent_location.col_index)
+            self.grid[hash(tent)] = tent
+            self.tents_dict[hash(tent)] = tent
+        # Decrease the allowed tents on the chosen node
+        constraint_row_node = self.get_row_constraint(tent_node)
+        constraint_col_node = self.get_col_constraint(tent_node)
+        if constraint_row_node.tents_number_required == 0:
+            self.board_state = BoardStateType.INVALID
+            return False
+        if constraint_col_node.tents_number_required == 0:
+            self.board_state = BoardStateType.INVALID
+            return False
+        constraint_col_node.tents_number_required -= 1
+        if constraint_col_node.is_satisfied():
+            self.remove_nodes_from_pos_list(
+                self.get_whole_col(constraint_col_node.col_index))
+        constraint_row_node.tents_number_required -= 1
+        if constraint_row_node.is_satisfied():
+            self.remove_nodes_from_pos_list(
+                self.get_whole_row(constraint_row_node.row_index))
 
-            
+        if len(self.tents_dict) > len(self.trees_dict):
+            self.board_state = BoardStateType.INVALID
+            return False
+
+        return True
+
     def print_board(self, mode="board_text"):
         for key in self.grid.keys():
             node = self.grid[key]
@@ -326,7 +372,6 @@ class BoardNode():
         self.grid[hash(tent)] = tent
         self.tents_dict[hash(tent)] = tent
         self.update_possible_location(tent_node=tent)
-
 
     def get_adjacent_nodes(self, node: PositionNode) -> Dict[int, PositionNode]:
         (row_index, col_index) = self.get_index_tuple_from_node(node)
@@ -413,4 +458,31 @@ class GameNode():
         self.board = board
         self.parent = parent
         self.action = action
+
+
+# * Wrapper of an action so we calculate its heuristics value
+class HeuristicNode(GameNode):
+    # Put an tent on a position on the board
+    def __init__(self, game_node: GameNode) -> None:
+        self.board = game_node.board
+        self.action = game_node.action
+        self.parent = game_node.parent
+        # The smaller, the better
+        self.heuristic_value = self.calculate_heuristic_value(game_node.board)
+
+    def __hash__(self):
+        return hash(self.game_node.board)
+
+    def __eq__(self, other):
+        return ((self.game_node.board) == (other.game_node.board))
+
+    def __lt__(self, other):
+        return self.heuristic_value < other.heuristic_value
+
+    def calculate_heuristic_value(self, board: BoardNode):
+        # Basic heuristic:
+        # * The sums of values of constraints
+        # * The smaller the sums --> Less options left for next tent --> better
+        # * The heapq lib is originaly an min heap as well
+        return functools.reduce(lambda a, b: a + b.tents_number_required, board.constraint_dict.values(), 0)
 
